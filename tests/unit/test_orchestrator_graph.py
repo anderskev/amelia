d51223a8a1cb2b9a77f6bafb8c07c72f73ebad0b
Kevin Anderson <anderskev@gmail.com>
2025-11-30 19:11:54 -0500

    test(cleanup): remove orchestrator_graph and orchestrator_memory - placeholder tests

--- tests/unit/test_orchestrator_graph.py
+++
@@ -1,8 +0,0 @@
-from amelia.core.orchestrator import create_orchestrator_graph
-
-
-def test_orchestrator_graph_structure():
-    graph = create_orchestrator_graph()
-    assert graph is not None
-    # Further assertions on graph structure would go here
-    # once nodes are added.
