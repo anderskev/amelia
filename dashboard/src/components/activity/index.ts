b0c4334886a55681a4d1dbbb8af2ddd5bbc98d43
Kevin Anderson <anderskev@gmail.com>
2026-01-07 00:36:46 -0500

    feat(dashboard): add activity components barrel export

---
+++ dashboard/src/components/activity/index.ts
@@ -0,0 +1,4 @@
+export { ActivityLogHeader } from './ActivityLogHeader';
+export { useActivityLogGroups } from './useActivityLogGroups';
+export type { StageGroup, VirtualRow, AgentStage } from './types';
+export { STAGE_ORDER, STAGE_LABELS } from './types';
